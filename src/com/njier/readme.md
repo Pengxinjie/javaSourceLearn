22f166891e608f8147a5df9468f57a99d9cda223
pengxinjie <1370639979@qq.com>
2020-03-25 21:44:15 +0800

    jdk源码

---
+++ src/com/njier/readme.md
@@ -0,0 +1,39 @@
+## jdk源码跟踪
+
+### 一.需要研究的类库
+
+1.io  
+2.lang  
+3.math  
+4.net  
+5.nio  
+6.time  
+7.util
+
+### 二.lang
+- 基础语法及包装类  
+
+主要：
+- reflect反射机制
+- annotation注解
+- 散列
+
+### 三.util
+
+主要：  
+- concurrent
+- function
+- stream 
+- 散列
+
+### 四.io
+普通io（阻塞式io）
+
+### 五.nio
+非阻塞io
+
+### 六.math
+
+### 七.time
+
+### 八.net
---
+++ src/com/njier/test/hashmap/Test.java
@@ -0,0 +1,20 @@
+package com.njier.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * @author Pengxinjie
+ * @DATE 2020/3/25 - 20:06
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String,Double> hashMap = new HashMap<>();
+
+        hashMap.put("k1",0.1);
+        hashMap.put("k2",0.2);
+        hashMap.put("k3",0.3);
+        hashMap.put("k4",0.4);
+
+
+    }
+}
