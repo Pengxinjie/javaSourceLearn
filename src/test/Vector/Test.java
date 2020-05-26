333543d6791a4d1cc36a71b76030a6252e85ad1b
pengxinjie <1370639979@qq.com>
2020-05-26 11:37:54 +0800

    Vector

---
+++ src/test/Vector/Test.java
@@ -0,0 +1,23 @@
+package test.Vector;
+
+import java.util.Vector;
+
+/**
+ * @author Pengxinjie
+ * @DATE 2020/5/25 - 21:29
+ */
+public class Test {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        //1.new Vector():初始化长度为10的数组，默认增量是0
+
+        v.add("1");
+        //2.add(E e):默认扩容为原来的两倍，如果手动指定了capacityIncrement的值，那么就按照指定的来
+
+        v.add(1,"2");
+        //3.add(int index,E e):考虑扩容-->移动元素-->添加-->元素个数增加
+
+        v.remove(1);
+        //4.remove(int index):计算移动元素的个数
+    }
+}
